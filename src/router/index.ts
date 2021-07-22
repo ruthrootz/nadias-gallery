a39a9dea73f8d9a07c998c5b8fb4bfc42c37419c
ruthrootz <ruthrootz@users.noreply.github.com>
2021-07-22 19:10:26 -0400

    cleanup router file

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,27 +1,25 @@
 import Vue from 'vue';
 import VueRouter, { RouteConfig } from 'vue-router';
-import Home from '../views/Home.vue';
+import Gallery from '../views/Gallery.vue';
 
 Vue.use(VueRouter);
 
 const routes: RouteConfig[] = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home,
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
-  },
+    {
+        path: '/',
+        name: 'Nadia\'s Gallery',
+        component: Gallery,
+    },
+    // {
+        // path: '/about',
+        // name: 'About',
+        // component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    // },
 ];
 
 const router = new VueRouter({
-  routes,
+    routes,
 });
 
 export default router;
+
