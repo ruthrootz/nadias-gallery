847fbca191abd22d3cfc4cc6720bbc57bd004dc9
ruthrootz <ruthrootz@users.noreply.github.com>
2021-07-24 11:30:24 -0400

    add to nav

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,9 @@
                     <router-link to="/">Nadia's Gallery</router-link>
                 </b-navbar-brand>
                 <b-navbar-nav class="ml-auto">
-                    <b-nav-item></b-nav-item>
+                    <b-nav-item>Etsy</b-nav-item>
+                    <b-nav-item>Upwork</b-nav-item>
+                    <b-nav-item>Instagram</b-nav-item>
                 </b-navbar-nav>
             </b-navbar>
         </div>
@@ -42,6 +44,11 @@
 
 #nav .navbar {
     background-color: #F2E9E4 !important;
+}
+
+#nav .nav-link {
+    font-weight: normal;
+    font-size: 2rem;
 }
 
 </style>
