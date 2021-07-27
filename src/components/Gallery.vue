ff8446d81c4a2c3ad8f91de744c4fcbc30beeed8
ruthrootz <ruthrootz@users.noreply.github.com>
2021-07-27 10:07:25 -0400

    added components folder

---
+++ src/components/Gallery.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="gallery">
+    </div>
+</template>
+
+<script lang="ts">
+
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Gallery extends Vue {
+
+    @Prop() private msg!: string;
+
+}
+
+</script>
+
+<style scoped>
+
+h3 {
+    margin: 40px 0 0;
+}
+
+ul {
+    list-style-type: none;
+    padding: 0;
+}
+
+li {
+    display: inline-block;
+    margin: 0 10px;
+}
+
+a {
+    color: #42b983;
+}
+
+</style>
+
