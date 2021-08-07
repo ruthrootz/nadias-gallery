b081cd72a85847bc58c5cbfb5b25ddf90e07716a
ruthrootz <ruthrootz@users.noreply.github.com>
2021-08-07 10:34:46 -0400

    allImages

--- src/components/Gallery.vue
+++ src/components/Gallery.vue
@@ -2,14 +2,14 @@
     <b-container fluid class="gallery">
         <div class="section">
             <b-card-group>
-            <b-card-group>
-                <b-card v-for="image in imagesPartOne"
-                        class="shadow text-center art-card"
-                        :img-src="image.source"
-                        :img-alt="image.title"
-                        img-top>
-                </b-card>
-            </b-card-group>
+                <b-card-group>
+                    <b-card v-for="image in imagesPartOne"
+                            class="shadow text-center art-card"
+                            :img-src="image.source"
+                            :img-alt="image.title"
+                            img-top>
+                    </b-card>
+                </b-card-group>
                 <b-card v-for="image in imagesPartTwo"
                         class="shadow text-center art-card"
                         :img-src="image.source"
@@ -27,6 +27,29 @@ import { Component, Prop, Vue } from 'vue-property-decorator';
 
 @Component
 export default class Gallery extends Vue {
+
+    private allImages: any[] = [
+        {
+            source: 'https://placedog.net/500/500?random',
+            title: 'test 1',
+        },
+        {
+            source: 'https://placedog.net/500/500?random',
+            title: 'test 2',
+        },
+        {
+            source: 'https://placedog.net/500/500?random',
+            title: 'test 3',
+        },
+        {
+            source: 'https://placedog.net/500/500?random',
+            title: 'test 4',
+        },
+        {
+            source: 'https://placedog.net/500/500?random',
+            title: 'test 5',
+        },
+    ]; 
 
     private imagesPartOne: any[] = [
         {
