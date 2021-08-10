ac2e75fb05931b4360ddfdd290208a215ce3c1ae
ruthrootz <ruthrootz@users.noreply.github.com>
2021-08-10 12:19:11 -0400

    update gallery

--- src/components/Gallery.vue
+++ src/components/Gallery.vue
@@ -5,15 +5,15 @@
                 <b-card-group>
                     <b-card v-for="image in imagesPartOne"
                             class="shadow text-center art-card"
-                            :img-src="image.source"
-                            :img-alt="image.title"
+                            :img-src="image.src"
+                            :img-alt="image.caption"
                             img-top>
                     </b-card>
                 </b-card-group>
                 <b-card v-for="image in imagesPartTwo"
                         class="shadow text-center art-card"
-                        :img-src="image.source"
-                        :img-alt="image.title"
+                        :img-src="image.src"
+                        :img-alt="image.caption"
                         img-top>
                 </b-card>
             </b-card-group>
@@ -36,50 +36,55 @@ export default class Gallery extends Vue {
 
     private allImages: any[] = [
         {
-            source: 'https://placedog.net/500/500?random',
-            title: 'test 1',
+            src: 'https://placedog.net/500/500?random',
+            thumb: 'https://placedog.net/500/500?random', 
+            caption: 'test 1',
         },
         {
-            source: 'https://placedog.net/500/500?random',
-            title: 'test 2',
+            src: 'https://placedog.net/500/500?random',
+            thumb: 'https://placedog.net/500/500?random', 
+            caption: 'test 2',
         },
         {
-            source: 'https://placedog.net/500/500?random',
-            title: 'test 3',
+            src: 'https://placedog.net/500/500?random',
+            thumb: 'https://placedog.net/500/500?random', 
+            caption: 'test 3',
         },
         {
-            source: 'https://placedog.net/500/500?random',
-            title: 'test 4',
+            src: 'https://placedog.net/500/500?random',
+            thumb: 'https://placedog.net/500/500?random', 
+            caption: 'test 4',
         },
         {
-            source: 'https://placedog.net/500/500?random',
-            title: 'test 5',
+            src: 'https://placedog.net/500/500?random',
+            thumb: 'https://placedog.net/500/500?random', 
+            caption: 'test 5',
         },
     ]; 
 
     private imagesPartOne: any[] = [
         {
-            source: 'https://placedog.net/500/500?random',
-            title: 'test 1',
+            src: 'https://placedog.net/500/500?random',
+            caption: 'test 1',
         },
         {
-            source: 'https://placedog.net/500/500?random',
-            title: 'test 2',
+            src: 'https://placedog.net/500/500?random',
+            caption: 'test 2',
         },
         {
-            source: 'https://placedog.net/500/500?random',
-            title: 'test 3',
+            src: 'https://placedog.net/500/500?random',
+            caption: 'test 3',
         },
     ];
 
     private imagesPartTwo: any[] = [
         {
-            source: 'https://placedog.net/500/500?random',
-            title: 'test 4',
+            src: 'https://placedog.net/500/500?random',
+            caption: 'test 4',
         },
         {
-            source: 'https://placedog.net/500/500?random',
-            title: 'test 5',
+            src: 'https://placedog.net/500/500?random',
+            caption: 'test 5',
         },
     ];
 
